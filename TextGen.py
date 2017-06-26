50744ed21aecfeff487f7f9bd5472fc03d3ba57f
Daniel-02 <daniel_toledo02@hotmail.com>
2017-06-26 14:44:32 -0300

    TextGen
    ajustei os \t

--- TextGen.py
+++ TextGen.py
@@ -1,27 +1,17 @@
-for position in range(0,31):
-    print("case position = {0}:".format(position))
+for position in range(0,32):
+    print("\t\tcase position = {0}:".format(position))
     col = position%4
     lin = position/4
-    print("\tcase whois = 1:")
+    print("\t\t\tcase whois = 1:")
     if (position == 3 or position == 11 or position == 19 or position == 27 or position == 4 or position == 12  or position == 20):
-        print("\t\tmove(board,{0},{1});".format(position,position + 4))
     elif (col%2) == 0 and lin < 7:
-    	print("\t\tmove(board,{0},{1});".format(position,position + 4))
-    	print("\t\tmove(board,{0},{1});".format(position,position + 5))
     elif (col%2) == 1 and lin < 7:
-        print("\t\tmove(board,{0},{1});".format(position, position + 3))
-        print("\t\tmove(board,{0},{1});".format(position, position + 4))
-    else: print("\t\t;")
-    print("\tcase whois = 2:")
+    else: print("\t\t\t\t;")
+    print("\t\t\tcase whois = 2:")
     if (position == 11 or position == 19 or position == 27 or position == 4 or position == 12  or position == 20 or position == 28):
-        print("\t\tmove(board,{0},{1});".format(position,position - 4))
     elif (col%2) == 0 and lin >= 1:
-    	print("\t\tmove(board,{0},{1});".format(position,position - 4))
-    	print("\t\tmove(board,{0},{1});".format(position,position - 3))
     elif (col%2) == 1 and lin >= 1:
-        print("\t\tmove(board,{0},{1});".format(position, position - 5))
-        print("\t\tmove(board,{0},{1});".format(position, position - 4))
-    else: print("\t\t;")
-    print("\tcase TRUE:;\n\tesac;")
-print("case TRUE:;\nesac;")
+    else: print("\t\t\t\t;")
+    print("\t\t\tcase TRUE:;\n\t\t\tesac;")
+print("\t\tcase TRUE:;\n\t\tesac;")
 
